36658c2d5a3cd258d75917c303636081306584ae
Nick Pettit <nick@nickpettit.com>
2022-04-12 08:50:43 -0500

    Simplified a few transforms

--- Assets/Scripts/RollingMovement.cs
+++ Assets/Scripts/RollingMovement.cs
@@ -9,21 +9,24 @@ public class RollingMovement : MonoBehaviour
 
     void Start()
     {
+        // Gather components and variables that will be needed later
         m_rigidbody = GetComponent<Rigidbody>();
         m_startPosition = transform.position;
     }
 
     void FixedUpdate()
     {
+        // Every physics tick, add a force equal to the movement vector multiplied by speed
         m_rigidbody.AddForce(m_movementDirection * m_speed);
-        Debug.DrawLine(transform.position, transform.position + m_movementDirection, Color.blue);
 
+        // If the Y position of the sphere is below the respawn height, reset its position
         if (transform.position.y <= GameManager.RespawnHeight)
             ResetPosition();
     }
 
     public void ResetPosition()
     {
+        // Reset the velocity to zero and set the position back to start
         m_rigidbody.velocity = Vector3.zero;
         m_rigidbody.angularVelocity = Vector3.zero;
         transform.position = m_startPosition;
